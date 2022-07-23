251fc34e6b14536a8a7183ea1d6ecf09df1c2869
Jay Unewal <jayunewal@outlook.com>
2022-07-23 20:49:36 +0530

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Jay Unewal on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
